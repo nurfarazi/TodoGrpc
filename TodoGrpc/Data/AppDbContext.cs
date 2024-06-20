7914d91c21b5bc883080f4bfed18ad30a534415c
farazi <nur369188@gmail.com>
2024-06-20 18:51:00 +0600

    chore: Add SQLite database configuration for TodoGrpc project

---
+++ TodoGrpc/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
+using TodoGrpc.Models;
+
+namespace TodoGrpc.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ToDoItem> ToDoItems => Set<ToDoItem>();
+    }
+}
--- TodoGrpc/Program.cs
+++ TodoGrpc/Program.cs
@@ -1,11 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TodoGrpc.Data;
 using TodoGrpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite("Data Source=todo.db");
+});
 
-// Add services to the container.
 builder.Services.AddGrpc();
 
 var app = builder.Build();
