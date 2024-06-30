8438eae6bc3141b07efd5271afd6b0dbe0667057
nur farazi <nur369188@gmail.com>
2024-06-30 20:17:13 +0600

    chore: Enable CRUD operations for todo items in TodoService

--- Grpc-API-Service/Services/TodoService.cs
+++ Grpc-API-Service/Services/TodoService.cs
@@ -1,3 +1,4 @@
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using TodoGrpc.Data;
 using TodoGrpc.Models;
@@ -7,31 +8,65 @@ namespace Grpc_API_Service.Services;
 public class TodoService : TodoIt.TodoItBase
 {
     private readonly AppDbContext _dbContext;
-    
+
     public TodoService(AppDbContext dbContext)
     {
         _dbContext = dbContext;
     }
-    
+
     public override async Task<CreateResponse> Create(CreateRequest request, ServerCallContext context)
     {
         if (request.Title == string.Empty)
         {
             throw new RpcException(new Status(StatusCode.InvalidArgument, "Title cannot be empty"));
         }
-        
+
         var todo = new ToDoItem
         {
             Title = request.Title
         };
-        
+
         await _dbContext.AddAsync(todo);
         await _dbContext.SaveChangesAsync();
-        
+
         return await Task.FromResult(new CreateResponse
         {
             Id = todo.Title
         });
     }
+
+    public override async Task<ReadResponse> Read(ReadRequest request, ServerCallContext context)
+    {
+        if (request.Id == string.Empty)
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Id cannot be empty"));
+
+        var todo = await _dbContext.ToDoItems.FindAsync(request.Id);
+
+        if (todo == null)
+            throw new RpcException(new Status(StatusCode.NotFound, "Todo not found"));
+
+        return await Task.FromResult(new ReadResponse
+        {
+            Title = todo.Title
+        });
+    }
     
+    public override async Task<UpdateResponse> Update(UpdateRequest request, ServerCallContext context)
+    {
+        if (request.Id == string.Empty)
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Id cannot be empty"));
+
+        var todo = await _dbContext.ToDoItems.FindAsync(request.Id);
+
+        if (todo == null)
+            throw new RpcException(new Status(StatusCode.NotFound, "Todo not found"));
+
+        todo.Title = request.Title;
+        await _dbContext.SaveChangesAsync();
+
+        return await Task.FromResult(new UpdateResponse
+        {
+            Title = todo.Title
+        });
+    }
 }
