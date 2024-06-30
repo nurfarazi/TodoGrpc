35f177611058e18c2942c317d535cd29ab351581
nur farazi <nur369188@gmail.com>
2024-06-30 16:15:21 +0600

    Updated: Services

--- Grpc-API-Service/Program.cs
+++ Grpc-API-Service/Program.cs
@@ -4,11 +4,7 @@ using TodoGrpc.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
-// Add services to the container.
-//
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite("Data Source=todo.db");
@@ -20,6 +16,8 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<TodoService>();
+
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
--- Grpc-API-Service/Services/TodoService.cs
+++ Grpc-API-Service/Services/TodoService.cs
@@ -1,42 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Grpc.Core;
+using TodoGrpc.Data;
+using TodoGrpc.Models;
 
-namespace Grpc-API-Service.Services
+namespace Grpc_API_Service.Services;
+
+public class TodoService : TodoIt.TodoItBase
 {
-    public class TodoService
+    private readonly AppDbContext _dbContext;
+    
+    public TodoService(AppDbContext dbContext)
     {
-        private readonly AppDbContext _dbContext;
-        public TodoService(AppDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-
-        public async Task<TodoModel> Create(CreateTodoModel model, ServerCallContext context)
-        {
-            if (string.IsNullOrEmpty(model.Title))
-            {
-                throw new RpcException(new Status(StatusCode.InvalidArgument, "Title is required"));
-            }
-
-            var todo = new Todo
-            {
-                Title = model.Title,
-                Description = model.Description,
-                IsCompleted = model.IsCompleted
-            };
-
-            _dbContext.Todos.Add(todo);
-            await _dbContext.SaveChangesAsync();
-
-            return await Task.FromResult(new TodoModel
-            {
-                Id = todo.Id,
-                Title = todo.Title,
-                Description = todo.Description,
-                IsCompleted = todo.IsCompleted
-            });
-        }
+        _dbContext = dbContext;
     }
+    
+    public override async Task<CreateResponse> Create(CreateRequest request, ServerCallContext context)
+    {
+        if (request.Title == string.Empty)
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Title cannot be empty"));
+        }
+        
+        var todo = new ToDoItem
+        {
+            Title = request.Title
+        };
+        
+        await _dbContext.AddAsync(todo);
+        await _dbContext.SaveChangesAsync();
+        
+        return await Task.FromResult(new CreateResponse
+        {
+            Id = todo.Title
+        });
+    }
+    
 }
